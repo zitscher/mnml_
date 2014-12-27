9d6e0bf844d25d5402f8512e318385e122b9d4e7
Tobias Cichon <tobiascichon@googlemail.com>
2014-12-27 16:22:04 +0100

    adds footer height constant, applies sticky footer

--- footer.php
+++ footer.php
@@ -1,11 +1,5 @@
 <footer role="contentinfo">
-	<div class="container">
-		<div class="row">
-			<div class="col-12">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"" rel="generator">&copy; <?php echo date('Y'); ?> <?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a>
-			</div>
-		</div>
-	</div>
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"" rel="generator">&copy; <?php echo date('Y'); ?> <?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a>
 </footer>
 
 <?php wp_footer(); ?>
--- index.php
+++ index.php
@@ -4,34 +4,35 @@
 <?php get_template_part('head'); ?>
 
 <body <?php body_class(); ?>>
+	<div id="content">
 
-	<?php get_header(); ?>
+		<?php get_header(); ?>
 
-	<?php get_template_part('hero'); ?>
+		<?php get_template_part('hero'); ?>
 
-	<div id="content" class="container" role="main">
+		<div class="container" role="main">
 
-		<div class="row">
-			<div class="col-12">
-			<?php
-				if(is_page()) {
-					get_template_part('site');
-				}
-				else if(is_home() || is_archive()) {
-					get_template_part('posts');
-				}
-				else if(is_single()) {
-					get_template_part('post');
-				}
-				else if(is_404()) {
-					get_template_part('not_found');
-				}
-			?>
+			<div class="row">
+				<div class="col-12">
+				<?php
+					if(is_page()) {
+						get_template_part('site');
+					}
+					else if(is_home() || is_archive()) {
+						get_template_part('posts');
+					}
+					else if(is_single()) {
+						get_template_part('post');
+					}
+					else if(is_404()) {
+						get_template_part('not_found');
+					}
+				?>
+				</div>
 			</div>
 		</div>
 	</div>
 
 	<?php get_footer(); ?>
-
 </body>
 </html>
